bd403269fbd75d9d8542e76aece35c0e3a720720
Tasha Upchurch <TashaSkyUp@gmail.com>
2022-05-25 21:55:58 -0400

    bugfix missing os import

--- src/StorySquadAI/WebApi/database.py
+++ src/StorySquadAI/WebApi/database.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
 SQLALCHEMY_DATABASE_URL = "sqlite://"+os.getenv("DATABASE_PATH") + "sql_app.db"
 
