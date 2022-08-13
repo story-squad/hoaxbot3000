7efc9023bde5fa542ea78a31d83e0028938ad826
Tasha Upchurch <TashaSkyUp@gmail.com>
2022-08-13 18:26:28 -0400

    word_hoax_prompt model to support new wordhoax api end point

--- src/StorySquadAI/WebApi/schemas.py
+++ src/StorySquadAI/WebApi/schemas.py
@@ -1,10 +1,18 @@
 from typing import Optional
 from pydantic import BaseModel
-
+from typing import Literal
 
 class ResponseRecord(BaseModel):
     id: int
     response: str
     is_bot:bool
+
+class word_hoax_prompt(BaseModel):
+    api_key: str
+    prompt: str
+    correct_definition: str
+    bot_name: str
+    # def_type can be one of: thing, movie, person
+    def_type: Literal["thing", "movie", "person"]
 
 
